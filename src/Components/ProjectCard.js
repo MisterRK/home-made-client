3eea9b07ed612308ea3e74e3c82c4c4ac71e438c
MisterC1ean <robert.keller44@icloud.com>
2020-06-07 20:37:07 -0400

    Some basic styling to the project card

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
 
 const ProjectCard = props => {
   // console.log("Project Card Props =>", props)
   return(
-    
-    <div>
-      {props.project.title} by {props.user.name}
-    </div>
+<Card raised='true'>
+    <CardContent>
+      <Typography variant='h6'>
+        {props.project.title}
+      </Typography>
+      <Typography variany='subtitile1'>
+        by {props.user.name}
+      </Typography>
+    </CardContent>
+</Card>
   )
 
 }
