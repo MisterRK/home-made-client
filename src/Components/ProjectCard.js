8b6141946fb85f6bf9bb0d7a5d1433769a3f851c
MisterC1ean <robert.keller44@icloud.com>
2020-06-08 09:59:08 -0400

    Second project seed rendering correctly

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -2,16 +2,27 @@ import React from 'react'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
+import { makeStyles} from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    margin: '25px',
+    backgroundColor: '#9CC298'
+  }
+});
 
 const ProjectCard = props => {
   // console.log("Project Card Props =>", props)
+  const classes = useStyles();
+
   return(
-<Card raised='true'>
+<Card raised='true' className={classes.root}>
     <CardContent>
-      <Typography variant='h6'>
+      <Typography variant='h5'>
         {props.project.title}
       </Typography>
-      <Typography variany='subtitile1'>
+      <Typography variant='subtitle1'>
         by {props.user.name}
       </Typography>
     </CardContent>
